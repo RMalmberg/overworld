3fbd6e7dd406f8966dd7d91c54b526bdf7f98b27
Arianna O'Neil <oneilarianna@gmail.com>
2020-04-25 16:26:52 -0400

    added backdrop logic/attempting hooks - fixed how SignUpPage was called through Router.js - found a few more gameIds through Postman - added the landing page backdrop img cycle logic to SignUpPage.js

--- frontend/src/modules/app/components/register/SignUpPage.js
+++ frontend/src/modules/app/components/register/SignUpPage.js
@@ -1,5 +1,53 @@
+import React, { useEffect } from "react"; // hooks
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
+import { Backdrop, Footer } from "../";
+import { getBackdrop } from "../../../landing/actions";
+import { Backdrops as options } from "../../../landing/utils";
+
 const SignUpPage = () => {
-return(
-);
+
+const { backdrop } = useSelector(state => state.landing);
+const dispatch = useDispatch();
+
+useEffect(() => { // instead of writing a class you can use a hook
+
+/*
+* followed the same backdrop logic from the landing page
+*/
+if (Object.keys(backdrop).length === 0) {
+const game = options[Math.floor(Math.random() * options.length)];
+dispatch(getBackdrop(game.gameId));
+
+}
+});
+
+/*
+* also followed the same backdrop logic from the landing page
+*/
+return (
+    <>
+    // testing just regular text through react
+    <h1> Hello, this a test to make sure I am doing stuff right! </h1>
+      {Object.keys(backdrop).length > 0 && (
+        <Backdrop imageId={backdrop.imageId} />
+      )}
+      {Object.keys(backdrop).length > 0 && (
+        <section className="backdrop-name">
+          Backdrop from{" "}
+          <Link
+            to={{
+              pathname: `/games/${backdrop.slug}`,
+              state: backdrop.gameId
+            }}
+          >
+            {backdrop.name}
+          </Link>
+        </section>
+      )}
+      <Footer />
+    </>
+  );
 };
+
 export default SignUpPage;
