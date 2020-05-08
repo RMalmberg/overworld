84bb9d37a6d2adfe43cbe2c8c1f61b901b2511ab
Joshua keif <jkeif@student.bridgew.edu>
2020-05-08 16:12:54 -0400

    Added backdrop to sign in page

--- frontend/src/modules/app/components/login/LoginPage.js
+++ frontend/src/modules/app/components/login/LoginPage.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Grid, Container} from "semantic-ui-react";
 import { LoginForm } from "./Form";
+import { Backdrop, Footer } from "../";
+import { getBackdrop } from "../../../landing/actions";
 import "./styles.css";
+import {useDispatch, useSelector} from "react-redux";
+import Link from "react-router-dom/modules/Link";
 
 const LoginPage = () => {
   const options = { position: ["isLeft"] };
+  const { backdrop } = useSelector(state => state.landing);
+  const dispatch = useDispatch();
+
+    useEffect(() => { // instead of writing a class you can use a hook
+
+    /*
+    * followed the same backdrop logic from the landing page
+    */
+    if (Object.keys(backdrop).length === 0) {
+    const game = options[Math.floor(Math.random() * options.length)];
+    dispatch(getBackdrop(game.gameId));
+
+    }
+    });
   return (
     <>
         <Grid>
@@ -13,6 +31,20 @@ const LoginPage = () => {
             <LoginForm />
           </Container>
         </Grid>
+        {Object.keys(backdrop).length > 0 && (
+        <section className="backdrop-name">
+          Backdrop from{" "}
+          <Link
+            to={{
+              pathname: `/games/${backdrop.slug}`,
+              state: backdrop.gameId
+            }}
+          >
+            {backdrop.name}
+          </Link>
+        </section>
+      )}
+        <Footer />
     </>
   );
 };
