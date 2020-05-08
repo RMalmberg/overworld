dff7339829b7b7e6c35934a6e04fe43574d7fc80
Joshua keif <jkeif@student.bridgew.edu>
2020-05-08 15:04:08 -0400

    Removed Old commented code

--- frontend/src/modules/app/components/login/Form.js
+++ frontend/src/modules/app/components/login/Form.js
@@ -1,50 +1,8 @@
 import React, { useState } from "react";
 import { login } from "../../actions";
-import Error from "../errors/";
 import { Redirect } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Button, Form } from "semantic-ui-react";
-
-/* Original form
-export const LoginForm = ({
-  handleChange,
-  handleSubmit,
-  validateForm,
-  username,
-  password
-}) => (
-  <Form onSubmit={handleSubmit}>
-    <Form.Field>
-      <label>Username</label>
-      <input name="username" value={username} onChange={handleChange} />
-    </Form.Field>
-    <Form.Field>
-      <label>Password</label>
-      <input
-        type="password"
-        value={password}
-        name="password"
-        onChange={handleChange}
-      />
-    </Form.Field>
-    <Button
-      floated="right"
-      positive
-      fluid
-      type="submit"
-      disabled={!validateForm()}
-    >
-      Sign In
-    </Button>
-  </Form>
-);
-
-LoginForm.propTypes = {
-  handleChange: PropTypes.func.isRequired,
-  handleSubmit: PropTypes.func.isRequired,
-  validateForm: PropTypes.func.isRequired
-};
-*/
 
 export const LoginForm = () => {
   const defaultState = {
