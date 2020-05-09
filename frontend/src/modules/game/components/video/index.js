c7db58b0e6cff7c9cdcad0ce0f8ba69f477c3ad9
Brian Day <brian.day7593@gmail.com>
2020-05-09 15:40:27 -0400

    adding video components to the website.

---
+++ frontend/src/modules/game/components/video/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button,Modal,Icon } from 'semantic-ui-react';
+import PropTypes from "prop-types";
+import Video_holder from "./video";
+import "./styles.css";
+
+const Video = ({game})=>{
+  return(
+    <Modal className="video-con" basic size="small" trigger={
+      <Button className="trailer-button" basic fluid color="grey">
+        <Icon name="play circle outline"/>
+        Watch Trailer
+      </Button>}>
+      <Modal.Content className="modal-con"embed="true">
+        <Video_holder game={game}/>
+      </Modal.Content>
+    </Modal>
+  )
+}
+Video.propTypes = {
+    game: PropTypes.object.isRequired
+}
+export default Video;
